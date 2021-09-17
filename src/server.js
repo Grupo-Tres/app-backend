3f49cc91d4bf2aa1558e24b37d12eb81ee9fda7d
Geovani Brito <geovani.br@gmail.com>
2021-09-17 19:35:18 -0300

    feat: estrutura básica da API

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import helloRouter from '../src/api/router/hello-router'
+
+const app = express()
+const port = process.env.port || 5000
+
+app.get('/', (req, res) => {
+    res.status(200).send({"server_status": "ok"}).json
+}) 
+
+app.use('/api/v1/hello', helloRouter)
+
+app.listen(port, () => {
+    console.log(`Servidor executando no endereço http://localhost:${port}`)
+})
