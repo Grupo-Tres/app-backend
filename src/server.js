7a215723bc0d9f2b76474679119dc8160c29120c
Geovani Brito <geovani.br@gmail.com>
2021-11-02 22:49:43 -0300

    fix: correção da porta

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import userRouter from '../src/api/router/user-router'
 
 const app = express()
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
     res.status(200).send({"server_status": "ok"}).json
