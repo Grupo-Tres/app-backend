e7fe1e268acf052143022f62d5db86ea6c287416
Geovani Brito <geovani.br@gmail.com>
2021-11-09 01:30:25 -0300

    feat: rota da tabela categoria

---
+++ src/api/controller/categoria-controller.js
@@ -0,0 +1,51 @@
+import { PrismaClient } from "prisma/prisma-client";
+const prisma = new PrismaClient();
+
+class Categoria {
+  async post(req, res) {
+    const creatcategoria = await prisma.categoria.create({ data: req.body });
+    const resposta = {
+      status: "ok",
+      registro: creatcategoria,
+    };
+
+    res.send(resposta);
+  }
+
+  async get(req, res) {
+    const categoria = await prisma.categoria.findUnique({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.send(categoria).json;
+  }
+
+  async getAll(req, res) {
+    const usuarios = await prisma.categoria.findMany();
+    res.send(usuarios).json;
+  }
+
+  async put(req, res) {
+    const usuario = await prisma.categoria.update({
+      where: {
+        id: req.params.id,
+      },
+      data: req.body,
+    });
+    res.send(usuario);
+  }
+
+  async delete(req, res) {
+    const usuario = await prisma.categoria.delete({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.send({
+      delete: req.params.id,
+    });
+  }
+}
+
+module.exports = Categoria;
