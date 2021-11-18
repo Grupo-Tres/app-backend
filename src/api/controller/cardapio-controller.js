13c6364d500ce640a4f47259abf1338792bb8f15
Geovani Brito <geovani.br@gmail.com>
2021-11-18 00:58:29 -0300

    test: Excluída variável de mock para os testes

--- src/api/controller/cardapio-controller.js
+++ src/api/controller/cardapio-controller.js
@@ -3,7 +3,6 @@ const prisma = new PrismaClient();
 
 class Cardapio {
   async get(req, res) {
-
     const cardapio = await prisma.categoria.findMany({
       select: {
         id: true,
@@ -19,34 +18,13 @@ class Cardapio {
             tamanho: true,
             quantidade: true,
             disponivel: true,
-            preco: true
+            preco: true,
           },
         },
       },
-    })
+    });
 
-    const mock = [
-      {
-        id: "254be3fa-a2e0-4b7b-aa72-50a2d0670398",
-        sessaoName: "Pizza Salgada",
-        secaoId: "pizzasal",
-        produtos: [
-          {
-            id: "254be3fa-a2e0-4b7b-aa72-50a2d0670398",
-            nome: "Calabresa com Catupiry",
-            descricao: "Pizza de calbresa com catupiry",
-            foto: "/image/calabresa.jpg",
-            estoque: true,
-            tamanho: "grande",
-            quantidade: 10,
-            disponivel: true,
-            preco: 40.0,
-            categoriaId: "254be3fa-a2e0-4b7b-aa72-50a2d0670398",
-          },
-        ],
-      },
-    ];
-    res.setHeader('Access-Control-Allow-Origin', process.env.CORS_URL)
+    res.setHeader("Access-Control-Allow-Origin", process.env.CORS_URL);
     res.send(cardapio).json;
   }
 }
